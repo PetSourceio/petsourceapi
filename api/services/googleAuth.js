d89a237499cb7209efe113de59d1154ae77dd21f
vbcca <vybccal@gmail.com>
2018-06-19 21:08:36 +0300

    Add token logger

--- api/services/googleAuth.js
+++ api/services/googleAuth.js
@@ -1,10 +1,10 @@
 const config = require('../config');
 const GoogleSignIn = require('google-sign-in');
 const project = new GoogleSignIn.Project(config.googleClientId);
-console.log('!!!' + config.googleClientId);
 
 exports.validateToken = function(email, token, args, callback) {
   console.log(config.googleClientId);
+  console.log(token);
   project.verifyToken(token).then((jsonData) => {
       if (jsonData.email != email) {
         console.log('Bad token provided for user');
